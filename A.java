b7ac101fc5f55ca48251372e5bab72cac97bc875
Veerababu5136 <veerasreeesailam@gmail.com>
2024-07-08 10:16:32 +0530

    Update A.java

--- A.java
+++ A.java
@@ -6,6 +6,6 @@ class A {
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("Hello, World");
+        System.out.println("bhaskar");
     }
 }
